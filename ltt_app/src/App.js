84ec53f46ec8bfcd18f8e5f5388ec27a3b252e05
BoriLap <bori9691@gmail.com>
2022-04-16 22:17:33 +0900

    Front_ver0.0

---
+++ ltt_app/src/App.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+import {BrowserRouter as Router} from 'react-router-dom'
+import Home from './pages';
+
+function App() {
+  return (
+    <Router>
+      <Home />
+    </Router> 
+  );
+}
+
+export default App;
---
+++ ltt_app/src/pages/index.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import MainSection from '../components/MainSection'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+const Home = () => {
+    const[isOpen, setIsOpen] = useState(false)
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+  return (
+    <>
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+        <Navbar toggle={toggle} />
+        <MainSection />
+    </>
+  )
+}
+
+export default Home
