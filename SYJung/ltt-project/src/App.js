8b9e197c2dde0d80ec0e422b4105faefebee0213
yeobdoll <jsysu2846@naver.com>
2022-04-18 18:59:33 +0900

    Ltt_site[Modify]->Css+Imgs

--- SYJung/ltt-project/src/App.js
+++ SYJung/ltt-project/src/App.js
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import Navbar from "./components/navbar";
 import './app.css';
 import Main from "./components/main";
+import Upload from "./components/upload";
 
 function App() {
   return (
     <Fragment>
         <Navbar></Navbar>
         <Main></Main>
+        <Upload></Upload>
     </Fragment>
   );
 }
---
+++ SYJung/ltt-project/src/components/upload.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Upload extends Component {
+    render() {
+        return (
+            <section id="upload">
+                <div>
+                    Hello
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Upload;
