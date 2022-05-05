163272fd2ed2e1d5ea0cb8a8c59f33822902f306
yeobdoll <jsysu2846@naver.com>
2022-05-05 21:32:17 +0900

    Ltt_site[Modify]-> Connecting to a Router

--- SYJung/ltt-project/src/pages/mainPage.js
+++ SYJung/ltt-project/src/pages/mainPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from '../components/navbar';
+import {Link} from 'react-router-dom'
 
 class MainPage extends Component {
     render() {
@@ -10,7 +11,9 @@ class MainPage extends Component {
                     <i className="home-logo fa-solid fa-jet-fighter-up"></i>
                     <span className="home__title">Welcome to LTT</span>
                     <h2 className="home__description">Start diagnosing disease<br/>in facility crops!</h2>
-                    <button className="home__button">Start Diagnostics</button>
+                    <button className="home__button">
+                        <Link to='/upload'>Start Diagnostics</Link>
+                    </button>
                 </section>
             </>
         );
--- SYJung/ltt-project/src/pages/uploadPage.js
+++ SYJung/ltt-project/src/pages/uploadPage.js
@@ -1,36 +1,41 @@
 import React, { Component } from 'react';
+import Navbar from '../components/navbar';
 
 class UploadPage extends Component {
     render() {
         return (
-            <section id="upload">
+            <>
+                <Navbar></Navbar>
+                <section id="upload">
                 {/* <input type="file" /> */}
-                <div className="upload">
-                    <div className="upload-files">
-                    <header>
-                    <p>
-                        <i className="fa fa-cloud-upload" aria-hidden="true"></i>
-                        <span className="up">up</span>
-                        <span className="load">load</span>
-                    </p>
-                    </header>
-                    <div className="body" id="drop">
-                    <i className="fa fa-file-text-o pointer-none" aria-hidden="true"></i>
-                    <p className="pointer-none"><b>Drag and drop</b> files here <br /> or <a href="" id="triggerFile">browse</a> to begin the upload</p>
+                    <div className="upload">
+                        <div className="upload-files">
+                            <header>
+                                <p>
+                                    <i className="fa fa-cloud-upload" aria-hidden="true"></i>
+                                    <span className="up">up</span>
+                                    <span className="load">load</span>
+                                </p>
+                            </header>
+                            <div className="body" id="drop">
+                                <i className="fa fa-file-text-o pointer-none" aria-hidden="true"></i>
+                                <p className="pointer-none"><b>Drag and drop</b> files here <br /></p>
+                                {/* or <a href="" id="triggerFile">browse</a> to begin the upload</p> */}
                                 <input type="file" multiple="multiple" />
-                    </div>
-                    <footer>
-                    <div className="divider">
-                        {/* <span><AR>FILES</AR></span> */}
-                    </div>
-                    <div className="list-files">
-                        {/* <!--   template   --> */}
-                    </div>
+                            </div>
+                            <footer>
+                                <div className="divider">
+                                    {/* <span><AR>FILES</AR></span> */}
+                                </div>
+                            <div className="list-files">
+                                {/* <!--   template   --> */}
+                            </div>
                                 <button className="importar">UPDATE FILES</button>
-                    </footer>
+                            </footer>
+                        </div>
                     </div>
-                </div>
-            </section>  
+                </section>  
+            </>
         );
     }
 }
