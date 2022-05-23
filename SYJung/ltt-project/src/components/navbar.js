bba9a90e864c4b2a83bb6058450e150b81576acd
yeobdoll <jsysu2846@naver.com>
2022-05-23 14:10:08 +0900

    LTT_site[Modify]->navbar, css (1) + gitignore [Modify]

--- SYJung/ltt-project/src/components/navbar.js
+++ SYJung/ltt-project/src/components/navbar.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 class Navbar extends Component {
     render() {
         return (
             <section id="navbar">
-                <i className="navbar-logo fa-solid fa-fan"></i>
-                <span>LTT</span>
-                <span></span>
+                <div className="navbar__logo">
+                    <i className="navbar-logo fa-solid fa-fan"></i>
+                    <Link to='/'>LTT</Link>
+                </div>
+                <ul className="navbar__menu">
+                    <li className="navbar__menu__item">Home</li>
+                    <li className="navbar__menu__item">About</li>
+                    <li className="navbar__menu__item">Contact</li>
+                </ul>
             </section>
         );
     }
