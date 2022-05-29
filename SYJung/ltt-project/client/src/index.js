8ad4ba8f645c6d576415b08e09c7fc08f9b826fd
yeobdoll <jsysu2846@naver.com>
2022-05-30 02:18:41 +0900

    Ltt_site[Modify]->comment

--- SYJung/ltt-project/client/src/index.js
+++ SYJung/ltt-project/client/src/index.js
@@ -6,11 +6,11 @@ import '@fortawesome/fontawesome-free/js/all.js';
 // import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 // yarn add @fortawesome/fontawesome-free
-// npm install react-router-dom
+// yarn add react-router-dom
 // yarn add styled-components
 // yarn add react-icons
 
-// yarn add express
+// yarn add express (프로젝트 폴더 위치에서 명령어 실행)
 /* 
 >> Express는 node.js의 핵심 모듈인 http와 connect컴포넌트를 기반으로 하는 웹 프레임워크로,
 개발을 빠르고 손쉽게 할 수 있도록 도와주는 역할을 함. 이는 미들웨어 구조 때문에 가능함
--- SYJung/ltt-project/server/server.js
+++ SYJung/ltt-project/server/server.js
@@ -1,12 +1,12 @@
-// yarn global add nodemon
-// yarn add concurrently --dev (dev를 붙이면 local에서만 사용하겠다는 의미)
+// yarn add nodemon --dev (dev를 붙이면 local에서만 사용하겠다는 의미)
+// yarn add concurrently 
 // nodemon: node 명령어는 파일을 변경할 때마다 이전 명령어를 종료하고 다시 실행해야하는 불편함이 있는데 nodemon을 이용하면 바로바로 업데이트 가능
 // concurrently: react 서버와 node 서버를 동시에 실행시키기 위한 모듈
 
 // react와 node 서버 간에 데이터를 주고 받기 위해서는 프록시 모듈이 필요
-// yarn add http-proxy-middleware 프록시 설치
+// yarn add http-proxy-middleware 프록시 설치 (client)
 
-// yarn add axios
+// yarn add axios (client)
 
 const express = require('express');
 const app = express();
