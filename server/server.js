d81c3c7a76a51d69a93df9583fbbd48cadb413c3
YangDongJae <ydj9805@me.com>
2022-05-30 02:35:14 +0900

    real default setting

--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.Port || 8080;
 const test = require('.//Router/test');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api' (req,res) => {
-  res
-})
-// app.get('/api/yolov5' (req,res) => {
-//   res.send([
-    
-//   ]);
-// });
+app.get('/api/hello', (req,res) => {
+  res.send({message: "HELLO WORLD!"})
+});
+
+app.listen(port, () => console.log('Listening on port ${port}'));
