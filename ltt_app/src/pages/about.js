893efd2daa84115f79cb5b2508019834925ad4e9
BoriLab <42792735+BoriLab@users.noreply.github.com>
2022-06-01 15:05:58 +0900

    Update about.js
    이전에 사용했던 컴포넌트 import 삭제

--- ltt_app/src/pages/about.js
+++ ltt_app/src/pages/about.js
@@ -4,7 +4,6 @@ import InfoAboutus from '../components/InfoAboutus'
 import { homeObjOne } from '../components/InfoSection/Data'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
-import ImgAx from '../components/Axiosimg'
 const About = () => {
     const[isOpen, setIsOpen] = useState(false)
     const toggle = () => {
@@ -16,7 +15,6 @@ const About = () => {
         <Sidebar isOpen={isOpen} toggle={toggle} />
         <Navbar toggle={toggle} />
         <InfoAboutus {...homeObjOne}/>
-        <ImgAx />
         <Footer />
     </>
   )
